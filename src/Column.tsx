61ff3f597f05762959752b4fd560b029942c8ab8
Brad Morgan <bkmorgan3@gmail.com>
2020-07-31 17:30:45 -0700

    builds basic create

---
+++ src/Column.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {AddNewItem} from './AddNewItem';
+import {ColumnContainer, ColumnTitle} from './styles';
+
+interface ColumnProps {
+  text: string 
+}
+
+
+
+export const Column = ({
+  text,
+  children
+}: React.PropsWithChildren<ColumnProps>) => {
+  return <ColumnContainer>
+    <ColumnTitle>
+      {text}
+    </ColumnTitle>
+    {children}
+    <AddNewItem 
+    toggleButtonText="+Add Another Task"
+    onAdd={console.log}
+    dark
+    />
+  </ColumnContainer>
+}
